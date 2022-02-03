76855da004378da6251d708d13ab4bad336151ba
Santosh Ghatani <santght90@gmail.com>
2022-02-03 12:27:31 +0545

    staticfile

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express=require("express");
 const mongoose=require("mongoose");
 require("./database/database");
+const cors = require("cors");
+
+app.use(express.static(__dirname+'/view/images'));
 
 const app=express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
