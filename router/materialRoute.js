12f7482ad25bfde091f6a13c198e23116130dff3
Santosh Ghatani <santght90@gmail.com>
2022-02-16 03:01:08 +0545

    materialModelUpdate

--- router/materialRoute.js
+++ router/materialRoute.js
@@ -8,7 +8,7 @@ const multer = require("multer");
 const upload = require("../uploads/file");
 
 //adding new materials details
-router.post('/material/add', auth.verifyUser, upload.single('material_image'), function(req,res){
+router.post('/material/add', auth.verifyUser, function(req,res){
     const materialName = req.body.materialName;
     material.findOne({materialName : materialName})
     .then(function(cdata){
@@ -20,12 +20,12 @@ router.post('/material/add', auth.verifyUser, upload.single('material_image'), f
         {   
             const materialPrice = req.body.materialPrice;
             const materialQuantity = req.body.materialQuantity;
-            const materialImage = req.file.filename;
+            // const materialImage = req.file.filename;
             const data = new material({
                 materialName : materialName,
                 materialPrice : materialPrice,
                 materialQuantity : materialQuantity,
-                materialImage : materialImage
+                // materialImage : materialImage
             });
             data.save()
             .then(function(){
@@ -43,6 +43,7 @@ router.post('/material/add', auth.verifyUser, upload.single('material_image'), f
 
 //updating material details
 router.put('/material/update', auth.verifyUser, function(req,res){
+    const mid = req.body.mid;
     const materialName = req.body.materialName;
     const materialPrice = req.body.materialPrice;
     const materialQuantity = req.body.materialQuantity;
@@ -73,7 +74,7 @@ router.delete('/material/delete/:mid', auth.verifyUser, function(req,res){
 })
 
 //view all the materials
-router.get('/material/view',function(req,res){
+router.get('/material/view', function(req,res){
     material.find()
     .then(function(result){
         res.json(result)
@@ -82,6 +83,19 @@ router.get('/material/view',function(req,res){
         res.json({message : "Something went wrong"})
     })
 })
+
+// view single material
+router.get('/material/single/:mid', auth.verifyUser, function(req,res){
+    const mid = req.params.mid;
+     material.findOne({_id : mid})
+     .then(function(result){
+         res.json(result)
+     })
+     .catch(function(){
+         res.json({message : "something went wrong"})
+     })
+})
+
 
 router.put('/material/image/upload/:mid', auth.verifyUser, upload.single('material_image'), function(req,res){
     const mid = req.params.mid;
